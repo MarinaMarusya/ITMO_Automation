2274a2cae3f00477e923b1db90e33fb6a59e81ac
Marina <marinamuba@mail.ru>
2024-02-03 19:41:42 +0300

    first commit

---
+++ test_hw.py
@@ -0,0 +1,6 @@
+def test_passing1():
+    assert ('home', 'work') == ('home', 'work')
+def test_passing2():
+    assert not 'QA' == 'QC'
+def test_passing3():
+    assert not (1, 1, 2, 3, 5) == (2, 3, 5)
